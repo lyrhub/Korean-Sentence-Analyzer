af152b751164bb5e04905fe5d8e49f6e85859c80
Will Li <Will_Li@jabil.com>
2026-06-26 15:55:32 +0800

    fix: set base to / for custom domain deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  base: '/Korean-Sentence-Analyzer/',
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: {
